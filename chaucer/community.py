600651eac23c96837788766b9527dfe4b54bc67b
Tiago Tresoldi <tiago.tresoldi@lingfil.uu.se>
2023-11-02 09:10:19 +0100

    Improve cote quality

--- chaucer/community.py
+++ chaucer/community.py
@@ -5,10 +5,10 @@ Performs community detection on preprocessed orthographic tabular data.
 """
 
 # Import Python standard libraries
-from typing import *
 import argparse
 import logging
 import random
+from typing import Any, Dict, Union
 
 # Import 3rd-party libraries
 from sklearn.cluster import AffinityPropagation
@@ -18,121 +18,187 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def cluster_affinity(df: pd.DataFrame):
-    clustering = AffinityPropagation(random_state=5).fit(df)
+
+def cluster_affinity(df: pd.DataFrame) -> np.ndarray:
+    """
+    Perform clustering using the Affinity Propagation algorithm.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input dataframe for clustering.
+
+    Returns
+    -------
+    np.ndarray
+        An array of cluster labels.
+    """
+    try:
+        clustering = AffinityPropagation(random_state=5).fit(df)
+    except Exception as e:
+        logging.error(f"Error in Affinity Propagation clustering: {e}")
+        raise
 
     return clustering.labels_
 
 
-def cluster_random(df: pd.DataFrame):
+def cluster_random(df: pd.DataFrame) -> list:
+    """
+    Assign random cluster labels to data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input dataframe for clustering.
+
+    Returns
+    -------
+    list
+        A list of random cluster labels.
+    """
     rows = len(df.index)
     clusters = random.randint(2, int(rows / 2))
 
     return [random.randint(0, clusters) for _ in range(rows)]
 
 
-def pca(data: pd.DataFrame, n_components: Union[int, str]):
+def pca(data: pd.DataFrame, n_components: Union[int, str]) -> pd.DataFrame:
     """
-    Perform standard PCA decomposition.
+    Perform PCA decomposition on the data.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The input data for PCA.
+    n_components : Union[int, str]
+        The number of components to keep.
+
+    Returns
+    -------
+    pd.DataFrame
+        The dataframe containing the principal components.
     """
-
-    # Run PCA, extract components, and build new dataframe; the name of the columns must be computed
-    # after the components are collected, as the `mle` argument value can lead to size
-    # that cannot be predicted beforehand
-    pca_decomp = PCA(n_components=n_components)
-    components = pca_decomp.fit_transform(data)
-    colnames = [f"pc{idx+1}" for idx in range(len(components[0]))]
-    principal = pd.DataFrame(data=components, columns=colnames, index=data.index.values)
-
-    print(pca_decomp.explained_variance_ratio_)
+    try:
+        pca_decomp = PCA(n_components=n_components)
+        components = pca_decomp.fit_transform(data)
+        colnames = [f"pc{idx+1}" for idx in range(components.shape[1])]
+        principal = pd.DataFrame(data=components, columns=colnames, index=data.index.values)
+        logging.info(f"Explained variance ratio: {pca_decomp.explained_variance_ratio_}")
+    except Exception as e:
+        logging.error(f"Error in PCA decomposition: {e}")
+        raise
 
     return principal
 
 
-def plot(principal, labels):
-    mss = principal.index.values
+def plot(principal: pd.DataFrame, labels: np.ndarray) -> None:
+    """
+    Plot the data with labels.
 
-    fig = plt.figure(figsize=(20, 20))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_xlabel("Principal Component 1", fontsize=15)
-    ax.set_ylabel("Principal Component 2", fontsize=15)
-    # ax.set_title("2 component PCA", fontsize=20)
-    plt.scatter(principal.pc1, principal.pc2)
-    ax.grid()
+    Parameters
+    ----------
+    principal : pd.DataFrame
+        The dataframe with the principal components.
+    labels : np.ndarray
+        The cluster labels for each data point.
+    """
+    try:
+        mss = principal.index.values
+        fig, ax = plt.subplots(figsize=(20, 20))
+        ax.set_xlabel("Principal Component 1", fontsize=15)
+        ax.set_ylabel("Principal Component 2", fontsize=15)
+        ax.grid()
 
-    matrix = principal.to_numpy()
-    colors = plt.cm.rainbow(np.linspace(0, 1, max(labels) + 1))
-    for i in range(len(matrix)):
-        x = matrix[i][0]
-        y = matrix[i][1]
-        plt.plot(x, y, "bo", c=colors[labels[i]])
-        plt.text(x * (1 + 0.03), y * (1 + 0.03), mss[i], fontsize=8)
+        matrix = principal.to_numpy()
+        colors = plt.cm.rainbow(np.linspace(0, 1, max(labels) + 1))
+        for i in range(len(matrix)):
+            x, y = matrix[i][0], matrix[i][1]
+            ax.scatter(x, y, c=[colors[labels[i]]])
+            ax.text(x * (1 + 0.03), y * (1 + 0.03), mss[i], fontsize=8)
 
-    plt.show()
+        plt.show()
+    except Exception as e:
+        logging.error(f"Error in plotting: {e}")
+        raise
 
 
 def read_dataframe(filename: str) -> pd.DataFrame:
     """
-    Read a dataframe prepared with `convert_json.py`.
-    """
+    Read a dataframe from a tab-delimited file.
 
-    df = pd.read_csv(filename, delimiter="\t", encoding="utf-8", index_col=0)
+    Parameters
+    ----------
+    filename : str
+        The path to the file to read.
+
+    Returns
+    -------
+    pd.DataFrame
+        The dataframe read from the file.
+    """
+    try:
+        df = pd.read_csv(filename, delimiter="\t", encoding="utf-8", index_col=0)
+    except FileNotFoundError:
+        logging.error(f"File {filename} not found.")
+        raise
+    except Exception as e:
+        logging.error(f"Error reading the dataframe: {e}")
+        raise
 
     return df
 
 
-def parse_arguments() -> dict:
+def parse_arguments() -> Dict[str, Any]:
     """
     Parse command line arguments.
 
-    :return: The command-line arguments as a dictionary.
+    Returns
+    -------
+    Dict[str, Any]
+        The command-line arguments as a dictionary.
     """
-
-    # Obtain arguments as a dictionary
-    parser = argparse.ArgumentParser(
-        description="Prepare orthographic data for analysis."
-    )
+    parser = argparse.ArgumentParser(description="Prepare orthographic data for analysis.")
     parser.add_argument("input", type=str, help="The source JSON file for processing.")
     parser.add_argument(
-        "-s",
-        "--scale",
+        "-s", "--scale",
         type=str,
         choices=["none", "standard_nomean", "standard_mean"],
         default="none",
-        help="Whether to perform preprocessing scaling and of which kind (default: none)",
+        help="Whether to perform preprocessing scaling and of which kind (default: none)"
     )
     parser.add_argument(
-        "-d",
-        "--decompose",
+        "-d", "--decompose",
         type=str,
         choices=["none", "pca"],
         default="pca",
-        help="Whether to perform decomposition and of which kind (default: pca)",
+        help="Whether to perform decomposition and of which kind (default: pca)"
     )
-    # NOTE: the random method is only for testing/developing purposes!
     parser.add_argument(
-        "-c",
-        "--cluster",
+        "-c", "--cluster",
         type=str,
         choices=["affinity", "random"],
         default="affinity",
-        help="Whether to perform clustering and of which kind (default: affinity)",
+        help="Whether to perform clustering and of which kind (default: affinity)"
     )
-    arguments = vars(parser.parse_args())
-
-    return arguments
+    return vars(parser.parse_args())
 
 
-def main(arguments: dict):
+def main(arguments: Dict[str, Any]) -> None:
     """
-    Script entry point
-    """
+    Script entry point.
 
+    Parameters
+    ----------
+    arguments : Dict[str, Any]
+        The dictionary of command-line arguments.
+    """
     # Read tabular data as a pandas dataframe
     df = read_dataframe(arguments["input"])
 
-    # Perform scaling if requested (as the matrix is very sparse, we usually don't want this)
+    # Perform scaling if requested
     if arguments["scale"] == "standard_mean":
         df = StandardScaler().fit_transform(df)
     elif arguments["scale"] == "standard_nomean":
@@ -148,13 +214,16 @@ def main(arguments: dict):
     elif arguments["cluster"] == "random":
         labels = cluster_random(df)
 
-    # Plot results
-    print(list(zip(df.index.values, labels)))
+    # Log results
+    logging.info(f"Data points and their cluster labels: {list(zip(df.index.values, labels))}")
 
+    # Plot results
     plot(df, labels)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    args = parse_arguments()
-    main(args)
+    try:
+        args = parse_arguments()
+        main(args)
+    except Exception as e:
+        logging.error(f"An error occurred during the execution of the script: {e}")
