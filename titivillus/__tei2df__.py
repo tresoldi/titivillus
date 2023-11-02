e8a81731556e933116e608a120988c94472398af
Tiago Tresoldi <tiago.tresoldi@lingfil.uu.se>
2023-11-02 14:20:53 +0100

    Improve tei2df

--- titivillus/__tei2df__.py
+++ titivillus/__tei2df__.py
@@ -1,22 +1,38 @@
-import xml.etree.ElementTree as ET
+#!/usr/bin/env python3
+"""
+__tei2df__.py
+
+This script processes XML data to generate a CSV file containing reading variations
+from textual witnesses as recorded in the TEI XML file.
+"""
+
+# Standard library imports
 from collections import defaultdict
 import argparse
-import pandas as pd
-import logging
 import itertools
+import logging
+import xml.etree.ElementTree as ET
 from pathlib import Path
 
+# Third-party library imports
+import pandas as pd
 
-def get_most_common_reading(reading_frequencies: defaultdict, name: str) -> str:
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def get_most_common_reading(reading_frequencies, name):
     """
-    Get the most common reading for a given name.
+    Get the most common reading for a given name based on reading frequencies.
 
     Parameters
     ----------
-    reading_frequencies : defaultdict
+    reading_frequencies : defaultdict(int)
         A dictionary storing frequency count of readings for each name.
     name : str
-        The name for which to find the most common reading.
+        The name for which the most common reading is determined.
 
     Returns
     -------
@@ -26,222 +42,232 @@ def get_most_common_reading(reading_frequencies: defaultdict, name: str) -> str:
     return max(reading_frequencies[name], key=reading_frequencies[name].get)
 
 
-def generate_intermediate_data(args):
+def parse_xml_to_csv(input_xml):
     """
-    Main function to process XML data and write results to a CSV.
+    Parse the XML data and generate intermediate data suitable for analysis.
+
+    Parameters
+    ----------
+    input_xml : str
+        The file path of the input XML file.
+
+    Returns
+    -------
+    defaultdict
+        The intermediate data structure containing reading frequencies.
     """
+    # Load and parse the XML data
+    with open(input_xml, "r", encoding="utf-8") as file_handle:
+        xml_content = file_handle.read().replace("\f", "")
+    root = ET.fromstring(xml_content)
 
-    # Load the XML data
-    with open(args.input_xml, "r", encoding="utf-8") as h:
-        xml_data = h.read().replace("\f", "")
-    root = ET.fromstring(xml_data)
-
-    # Dictionary to store frequency count of readings for each name
+    # Initialize a dictionary to store frequency counts of readings
     reading_frequencies = defaultdict(lambda: defaultdict(int))
     namespace = {"ns": "http://www.tei-c.org/ns/1.0"}
 
-    # First pass: Count the frequencies of each reading for every name
-    for idx, app in enumerate(root.findall(".//ns:app", namespace)):
-        name = app.attrib["n"] + "-" + str(idx)
-        for rdg in app.findall("ns:rdg", namespace):
-            reading = rdg.text
-            reading_frequencies[name][reading] += 1
+    # Count frequencies of each reading for names
+    for index, app in enumerate(root.findall(".//ns:app", namespace)):
+        name = f"{app.attrib['n']}-{index}"
+        for reading_elem in app.findall("ns:rdg", namespace):
+            reading_text = reading_elem.text
+            reading_frequencies[name][reading_text] += 1
 
-    # List to collect all rows as dictionaries
-    csv_rows = []
-
-    # Extract readings and witnesses
-    for idx, app in enumerate(root.findall(".//ns:app", namespace)):
-        name_attr = app.attrib["n"]
-        token_index = str(idx)
-        most_common_reading = get_most_common_reading(
-            reading_frequencies, name_attr + "-" + token_index
+    # Extract data for CSV
+    csv_data = []
+    for index, app in enumerate(root.findall(".//ns:app", namespace)):
+        name_attribute = app.attrib["n"]
+        token_index = str(index)
+        common_reading = get_most_common_reading(
+            reading_frequencies, f"{name_attribute}-{token_index}"
         )
 
-        for rdg in app.findall("ns:rdg", namespace):
-            reading = rdg.text if rdg.text is not None else ""
-            varSeq = rdg.get("varSeq")
-            witnesses_list = [
+        for reading_elem in app.findall("ns:rdg", namespace):
+            reading_text = reading_elem.text or ""
+            reading_number = reading_elem.get("varSeq")
+            witnesses = [
                 idno.text
-                for wit in rdg.findall("ns:wit", namespace)
-                for idno in wit.findall("ns:idno", namespace)
+                for wit_elem in reading_elem.findall("ns:wit", namespace)
+                for idno in wit_elem.findall("ns:idno", namespace)
             ]
-            for witness in witnesses_list:
-                csv_rows.append(
+            for witness in witnesses:
+                csv_data.append(
                     {
                         "Token Index": token_index,
-                        "Name": name_attr,
-                        "Reading Number": varSeq,
-                        "Reading": reading,
+                        "Name": name_attribute,
+                        "Reading Number": reading_number,
+                        "Reading": reading_text,
                         "Witnesses": witness,
-                        "Most Common Reading": most_common_reading,
+                        "Most Common Reading": common_reading,
                     }
                 )
 
-    # build a data structure
-    data_structure = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-    for row in csv_rows:
-        token_index, name_attr, varSeq, reading, witness, most_common_reading = (
-            row["Token Index"],
-            row["Name"],
-            row["Reading Number"],
-            row["Reading"],
-            row["Witnesses"],
-            row["Most Common Reading"],
-        )
+    # Convert the list of dictionaries to a nested default dictionary
+    nested_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    for row in csv_data:
+        nested_dict[row["Most Common Reading"]][row["Reading"]][row["Witnesses"]] += 1
 
-        data_structure[most_common_reading][reading][witness] += 1
-
-    return convert_to_dict(data_structure)
+    return nested_dict
 
 
-def convert_to_dict(input_dict):
+def convert_nested_defaultdict_to_dict(nested_defaultdict):
     """
-    Recursively converts nested defaultdicts to standard dictionaries.
+    Recursively converts a nested defaultdict to a standard dictionary.
+
+    Parameters
+    ----------
+    nested_defaultdict : defaultdict
+        The nested defaultdict to convert.
+
+    Returns
+    -------
+    dict
+        The converted standard dictionary.
     """
-    if isinstance(input_dict, (defaultdict, dict)):
-        input_dict = {key: convert_to_dict(value) for key, value in input_dict.items()}
-    return input_dict
+    if isinstance(nested_defaultdict, (defaultdict, dict)):
+        return {
+            key: convert_nested_defaultdict_to_dict(value)
+            for key, value in nested_defaultdict.items()
+        }
+    return nested_defaultdict
 
 
-def process_raw(data: dict, smoothing: str, filter: bool) -> pd.DataFrame:
+def process_intermediate_data(raw_data, smoothing, filter_witnesses):
     """
-    Process raw data with observations.
+    Process the intermediate raw data into a pandas DataFrame.
+
+    Parameters
+    ----------
+    raw_data : dict
+        The intermediate raw data.
+    smoothing : str
+        The type of smoothing to apply to the data.
+    filter_witnesses : bool
+        Whether to filter out certain witnesses.
+
+    Returns
+    -------
+    pd.DataFrame
+        The processed data in a DataFrame.
     """
 
-    logging.debug(f"Processing data...")
+    def manuscript_passes_filter(ms_label):
+        return not any(char in ms_label for char in "-()")
 
-    def pass_filter(ms_label):
-        if "-" in ms_label:
-            return False
-        if "(" in ms_label:
-            return False
-
-        return True
-
-    # Collect all key/key pairs; also collect the total number of observations per proto-form
-    # for each manuscript, so that we can later compute the ratios
-    flatten = {}
-    form_obs = defaultdict(int)
-    for key, value in data.items():
-        for subkey, subvalue in value.items():
-            if filter:
-                flatten[f"{key}___{subkey}"] = {
-                    ms: reading for ms, reading in subvalue.items() if pass_filter(ms)
+    flatten_data = {}
+    form_observations = defaultdict(int)
+    for key, readings_dict in raw_data.items():
+        for subkey, witnesses_count in readings_dict.items():
+            valid_witnesses = (
+                {
+                    ms: count
+                    for ms, count in witnesses_count.items()
+                    if manuscript_passes_filter(ms)
                 }
-            else:
-                flatten[f"{key}___{subkey}"] = subvalue
-
-            for ms, count in subvalue.items():
-                if pass_filter(ms):
-                    form_obs[key, ms] += count
-
-    # Collect all manuscripts (it would be faster to do it in the loop above,
-    # but this is clearer)
-    mss = sorted(
-        set(
-            itertools.chain.from_iterable(
-                [list(value.keys()) for value in flatten.values()]
+                if filter_witnesses
+                else witnesses_count
             )
-        )
+            flatten_data_key = f"{key}___{subkey}"
+            flatten_data[flatten_data_key] = valid_witnesses
+
+            for manuscript, count in valid_witnesses.items():
+                form_observations[(key, manuscript)] += count
+
+    manuscripts = sorted(
+        set(itertools.chain.from_iterable(d.keys() for d in flatten_data.values()))
     )
+    readings = sorted(flatten_data)
 
-    # Build the Pandas dataframe (again, it would be faster in the loop above, but this
-    # makes future manipulations easier)
-    readings = sorted(list(flatten))
     if smoothing == "mle":
-        vectors = []
-        for ms in mss:
-            vector = []
-            for reading in readings:
-                total = form_obs.get((reading.split("___")[0], ms))
-                if not total:
-                    vector.append(0)
-                else:
-                    vector.append(flatten[reading].get(ms, 0) / total)
-
-            vectors.append(vector)
+        vectors = [
+            [
+                flatten_data[reading].get(ms, 0)
+                / form_observations.get((reading.split("___")[0], ms), 1)
+                for reading in readings
+            ]
+            for ms in manuscripts
+        ]
     elif smoothing == "none":
-        vectors = [[flatten[reading].get(ms, 0) for reading in readings] for ms in mss]
+        vectors = [
+            [flatten_data[reading].get(ms, 0) for reading in readings]
+            for ms in manuscripts
+        ]
 
-    # Build dataframe from vectors
-    df = pd.DataFrame(vectors, mss, readings)
-
-    return df
+    dataframe = pd.DataFrame(vectors, index=manuscripts, columns=readings)
+    return dataframe
 
 
-def write_tabular(data: pd.DataFrame, output_file: str, overwrite: bool):
+def write_dataframe_to_csv(dataframe, output_path, overwrite):
     """
-    Write results dataframe to disk.
+    Write the DataFrame to a CSV file.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The DataFrame containing the data to be written to CSV.
+    output_path : str
+        The path to the output CSV file.
+    overwrite : bool
+        If true, will overwrite the existing file at the output path.
     """
-
-    # Write data to disk, if possible/allowed
-    if Path(output_file).is_file():
-        if overwrite:
-            logging.debug(f"Output file `{output_file}` already exists, overwriting...")
-            write = True
-        else:
-            logging.debug(
-                f"Output file `{output_file}` already exists, skipping (use `--overwrite`?)..."
-            )
-            write = False
-    else:
-        logging.debug(f"Writing to output file `{output_file}`...")
-        write = True
-
-    if write:
-        data.to_csv(output_file, sep="\t", encoding="utf-8", float_format="%.4f")
-        logging.debug(
-            "Wrote file (note that in some systems you might need to wait some seconds for the flushing)."
+    output_file = Path(output_path)
+    if output_file.is_file() and not overwrite:
+        logging.info(
+            f"Output file `{output_path}` already exists. Use `--overwrite` to enable overwriting."
         )
+        return
+
+    dataframe.to_csv(output_path, sep="\t", encoding="utf-8", float_format="%.4f")
+    logging.info(f"Data written to {output_path}")
 
 
-def parse_args():
+def parse_command_line_arguments():
     """
     Parse command line arguments.
 
     Returns
     -------
     argparse.Namespace
-        Parsed arguments.
+        The namespace containing the parsed arguments.
     """
     parser = argparse.ArgumentParser(
         description="Process XML data to generate a CSV of readings."
     )
     parser.add_argument("input_xml", type=str, help="Path to the input XML file.")
-    parser.add_argument(
-        "output",
-        type=str,
-        help="The output file (defaults to a name similar to input, without overwriting).",
-    )
+    parser.add_argument("output_csv", type=str, help="Path to the output CSV file.")
     parser.add_argument(
         "--overwrite",
         action="store_true",
-        help="Whether to overwrite an existing output file.",
+        help="Overwrite the output file if it exists.",
     )
     parser.add_argument(
-        "-s",
         "--smoothing",
-        type=str,
-        choices=["mle", "none"],
-        default="mle",
-        help="What kind of smoothing to apply (default: mle).",
+        choices=["none", "mle"],
+        default="none",
+        help="The smoothing technique to use.",
     )
     parser.add_argument(
-        "-f",
-        "--filter",
-        action="store_true",
-        help="Filter manuscript keeping only the main transcriptions.",
+        "--filter-witnesses", action="store_true", help="Filter out certain witnesses."
     )
-
     return parser.parse_args()
 
 
 def main():
-    args = parse_args()
-    intermediate_data = generate_intermediate_data(args)
-    data = process_raw(intermediate_data, args.smoothing, args.filter)
-    write_tabular(data, args.output, args.overwrite)
+    """
+    The main function to execute the script functionality.
+    """
+    args = parse_command_line_arguments()
+
+    logging.info("Processing XML data...")
+    intermediate_data = parse_xml_to_csv(args.input_xml)
+    converted_data = convert_nested_defaultdict_to_dict(intermediate_data)
+
+    logging.info("Processing intermediate data...")
+    dataframe = process_intermediate_data(
+        converted_data, args.smoothing, args.filter_witnesses
+    )
+
+    logging.info("Writing output CSV...")
+    write_dataframe_to_csv(dataframe, args.output_csv, args.overwrite)
 
 
 if __name__ == "__main__":
